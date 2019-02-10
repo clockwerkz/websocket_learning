9ea17815fce6dc94adb61dfcb42764fdb7f576aa
clockwerkz <clockwerkz@gmail.com>
2019-02-10 00:05:12 -0800

    adds a broadcast newMessage for all connected clients thru io

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ const socket = io();
 socket.on('connect', function(){
     console.log('Connected to Server');
 
-    socket.emit('createMessage', {
-        from: 'Carlos',
-        text: 'Hello from the client!'
-    });
-
 });
 
 socket.on('newMessage', (message)=> {
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const http = require('http');
+const _ = require('lodash');
 const express = require('express');
 const socketIO = require('socket.io');
 
@@ -14,14 +15,15 @@ app.use(express.static(publicPath));
 io.on('connection', (socket)=> {
     console.log('New user connected');
 
-    socket.emit('newMessage', {
-        from: 'Carlos',
-        text: 'Hey can you meet at Barnes and Noble?',
-        createdAt: Date.now()
-    });
 
     socket.on('createMessage', (message)=> {
         console.log('createMessage:', message);
+        const { from, text } = _.pick(message, ['from','text']);
+        io.emit('newMessage', {
+            from,
+            text,
+            createdAt: Date.now()
+        });
     })
 
     socket.on('disconnect', ()=> {
