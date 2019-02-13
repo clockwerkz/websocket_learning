09288ddda9f143432a127ba4f4a8afb430754567
clockwerkz <clockwerkz@gmail.com>
2019-02-13 00:28:43 -0800

    adds testing for generateLocationMessage

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 const expect = require('expect');
-const { generateMessage } = require('./message');
+const { generateMessage, generateLocationMessage } = require('./message');
 
 describe('generateMessage function:', ()=> {
     it('should create a new message when called with two values', (done)=> {
@@ -14,3 +14,18 @@ describe('generateMessage function:', ()=> {
     });
 
 });
+
+describe('generateLocationMessage', ()=> {
+    it('should generate correct location object', (done)=> {
+        const from = 'User';
+        const coords = {
+            latitude: 1,
+            longitude: 1
+        }
+        const message = generateLocationMessage({from, coords});
+        expect(message.from).toBe(from);
+        expect(message.url).toBe(`https://www.google.com/maps/@?api=1&map_action=map?center=${coords.latitude},${coords.longitude}`);
+        expect(typeof message.createdAt).toBe('number');
+        done();
+    });
+})
